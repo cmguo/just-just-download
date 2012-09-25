55fa83cfa678d043ae505c442278756a40d579fb
isxxguo <isxxguo@pptv.com>
2012-09-25 03:48:39 +0000

    /ppbox/branches/1.2.0 /src/ppbox/download/Manager.h 删除对certify的依赖
    git-svn-id: http://svn01/repositories/r01/ppbox/branches/1.2.0@4993 b3a1389e-2159-5c43-9038-463bf877d0d9

--- Manager.h
+++ Manager.h
@@ -5,14 +5,6 @@
 #include "ppbox/download/Downloader.h"
 #include "ppbox/download/CommonType.h"
 
-#include <vector>
-
-#include <ppbox/common/CommonModuleBase.h>
-#include <ppbox/demux/base/DemuxerType.h>
-#ifndef  PPBOX_DISABLE_CERTIFY
-#include <ppbox/certify/CertifyUserModule.h>
-#endif
-
 #include <boost/thread.hpp>
 
 namespace ppbox
@@ -23,34 +15,20 @@ namespace ppbox
  		typedef void* DownloadHander;
 
 		class Manager
-#ifdef  PPBOX_DISABLE_CERTIFY			
             : public ppbox::common::CommonModuleBase<Manager>
-#else			
-            : public ppbox::certify::CertifyUserModuleBase<Manager>
-#endif            
         {
-        public:
-            // 进入认证成功状态
-            virtual void certify_startup();
-
-            // 退出认证成功状态
-            virtual void certify_shutdown(
-                boost::system::error_code const & ec);
-
-            // 认证出错
-            virtual void certify_failed(
-                boost::system::error_code const & ec);
-
         public:
             Manager(
                 util::daemon::Daemon & daemon);
 
             ~Manager();
 
+        public:
             virtual boost::system::error_code startup();
 
             virtual void shutdown();
 
+        public:
             Downloader * add(
                 std::string const & playlink,
                 std::string const & format,
@@ -62,28 +40,20 @@ namespace ppbox
                 Downloader * download_hander,
                 boost::system::error_code & ec);
 
-            void add_call_back(
-                Downloader * downloader,
-                boost::system::error_code const & ec);
-
             boost::system::error_code get_download_statictis(
                 Downloader * download_hander,
                 DownloadStatistic & download_statistic,
                 boost::system::error_code & ec);
 
-//            boost::uint32_t get_downloader_count(void) const;
-
             boost::system::error_code get_last_error(
                 DownloadHander const download_hander) const;
 
-            // 设置全局参数
-            void set_download_path(
-                char const * path);
+        private:
+            void add_call_back(
+                Downloader * downloader,
+                boost::system::error_code const & ec);
 
         private:
-            // config
-
-            std::string storage_path_;
             boost::mutex mutex_;
             std::vector<ppbox::download::DownloadInfo> info_vec_;
 			boost::condition_variable cond_;
